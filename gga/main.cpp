b2da79193bb3205df793ab51e9ca9e237a0d99e3
XiYe38324 <1215142591@qq.com>
2019-06-19 21:32:22 +0800

    Initial Commit

---
+++ gga/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  gga
+//
+//  Created by s20181106276 on 2019/6/19.
+//  Copyright © 2019 s20181106276. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
