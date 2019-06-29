aae48bffca8fbe1f1c9947985756470120da639f
XiYe38324 <1215142591@qq.com>
2019-06-29 14:59:09 +0800

    最终版本1

--- gga/main.cpp
+++ gga/main.cpp
@@ -6,20 +6,20 @@ using namespace std;
 
 struct Judge{
     string JudgeName;//评委姓名
-    int Score[3]={0,0,0};//评委打分
+    double Score[3]={0,0,0};//评委打分
 } J[7];
 struct Student{
     string Academy;//学院
     string StudentNumber;//学号
     string StudentName;//学生姓名
     string TelephoneNumber;//手机号
-    double score[7];
-    int average;
+    double Grade[7];//学生成绩
+    double Average;//平均分
 } S[3];
 
 int main()
 {
-    int a=0,b=0;
+    int a=0,b=0,c=0;
     
     ifstream Group1("/Users/den-o/Desktop/Group1.txt");
     while(!Group1.eof()&& a<7)
@@ -29,7 +29,6 @@ int main()
         a++;
     }
     Group1.close();
-    
     
     ifstream Group2("/Users/den-o/Desktop/Group2.txt");
     while(!Group2.eof()&& b<3)
@@ -44,11 +43,10 @@ int main()
     {
         for(int j=0;j<7;j++)
         {
-            S[i].score[j]=J[j].Score[i];
+            S[i].Grade[j]=J[j].Score[i];
         }
         cout<<endl;
     }
-    
     /*
     for(int i=0;i<3;i++)
     {
@@ -64,22 +62,35 @@ int main()
     */
     for(int i=0;i<3;i++)
     {
-        int sum=0;
-        int max=S[i].score[0],min=S[i].score[0];
+        double sum=0;
+        double max=S[i].Grade[0],min=S[i].Grade[0];
         for(int j=0;j<7;j++)
         {
-            if(S[i].score[j]>max)
-                max=S[i].score[j];
-            if(min>S[i].score[j])
-                min=S[i].score[j];
-            sum+=S[i].score[j];
+            if(S[i].Grade[j]>max)
+                max=S[i].Grade[j];
+            if(min>S[i].Grade[j])
+                min=S[i].Grade[j];
+            sum+=S[i].Grade[j];
         }
     // cout<<sum<<" "<<min<<" "<<max<<endl;
-          S[i].average=(sum-min-max)/5;
+          S[i].Average=(sum-min-max)/5;
         cout<<endl;
-    }
-  
     
+
+
+    ofstream Group3("/Users/den-o/Desktop/Group3.txt");
+    while(!Group3.eof()&& c<3)
+    {
+        Group3<<S[c].Academy<<" "<<S[c].StudentNumber<<" "<<S[c].StudentName<<" "<<S[c].TelephoneNumber<<" "<<S[c].Average<<endl;
+        //  cout << S[b].Academy<<" "<<S[b].StudentNumber<<" "<<S[b].StudentName<<" "<<S[b].TelephoneNumber<<endl;
+        c++;
+    }
+    Group2.close();
+    
+    for(int i=0;i<3;i++)
+    {
+        cout<<S[i].Academy<<" "<<S[i].StudentNumber<<" "<<S[i].StudentName<<" "<<S[i].TelephoneNumber<<" "<<S[i].Average<<endl;
+    }
     
     return 0;
 }
